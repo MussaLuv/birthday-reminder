125d4d0d83709a2e070b70edda3dcaa4c27c42b0
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-05-09 19:21:24 -0500

    Finished creating a practice project about birthday reminders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">Hello Guys</header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today!!!</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All!</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/README.md
@@ -0,0 +1,1 @@
+[https://uidesigndaily.com](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
